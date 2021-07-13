7323b6d52aa2acae0989188b0d702c89896498c4
Isaac <siphron0@gmail.com>
2021-07-13 12:15:17 +0200

    [ADD] Hangman Class

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pygame
-import os
+
+# Display
+import hangman
 
 pygame.init()
 WIDTH, HEIGHT = 800, 500
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman-ESGI!")
 
+# Images
+images = []
+for i in range(7):
+    images.append(pygame.image.load("images/Hangman-" + str(i) + ".png"))
+
 FPS = 60
 run = True
 clock = pygame.time.Clock()
 
 while run:
+    game = hangman.Hangman()
     clock.tick(FPS)
+    window.fill((255, 255, 255))
+    window.blit(images[game.get_hangman_status()], (0, 0))
+    pygame.display.update()
+
+    game.start_game()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
 
 pygame.quit()
---
+++ words.py
@@ -0,0 +1,26 @@
+import random
+
+from random_word import *
+
+r = RandomWords()
+words_list = r.get_random_words()
+
+
+def loadWordsToFile():
+    with open("words.txt", 'w') as f:
+        f.write(' '.join(words_list))
+
+
+def clearWordFile():
+    open("words.txt", "w").close()
+
+
+def get_random_word():
+    with open("words.txt", "r") as f:
+        allText = f.read()
+        words = list(map(str, allText.split()))
+        return random.choice(words)
+
+
+loadWordsToFile()
+print(get_random_word())
