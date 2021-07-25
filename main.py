912652acd289282605668ff337dce8086835994b
danxomename <isacramento@myges.fr>
2021-07-25 17:57:03 +0200

    update dictionaire + manage - et accent + histo

--- main.py
+++ main.py
@@ -21,9 +21,7 @@ for i in range(7):
 FONT = pygame.font.SysFont('Arial', 35)
 
 # Variables
-
 game_status = 0
-
 word = words.get_random_word().upper()
 guessed = []
 
@@ -39,6 +37,16 @@ for i in range(26):
     y = starty + ((i // 13) * (GAP + RADIUS * 2))
     letters.append([x, y, chr(A + i), True])
 
+def game_result(message):
+    with open("words.txt", 'a') as f:
+        f.write(": " + message + "\n")
+    pygame.time.delay(1000)
+    window.fill((255, 255, 255))
+    text = FONT.render(message, 1, BLACK)
+    window.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(3000)
+
 
 def draw():
     window.fill((255, 255, 255))
@@ -47,10 +55,14 @@ def draw():
     text = FONT.render("HANGMAN GAME", 1, BLACK)
     window.blit(text, (WIDTH / 2 - text.get_width() / 2, 20))
 
+
     displayed_word = ""
     for letter in word:
         if letter in guessed:
             displayed_word += letter + " "
+        elif letter == '-':
+            guessed.append('-')
+            displayed_word += "- "
         else:
             displayed_word += "_ "
     text = FONT.render(displayed_word, 1, BLACK)
@@ -92,6 +104,20 @@ def main():
                             if ltr not in word:
                                 game_status += 1
         draw()
+
+        won = True
+        for letter in word:
+            if letter not in guessed:
+                won = False
+                break
+
+        if won:
+            game_result("Gagné!")
+            break
+
+        if game_status == 6:
+            game_result("Perdu!")
+            break
 
 
 while True:
--- words.py
+++ words.py
@@ -1,25 +1,40 @@
-import random
-
-from random_word import *
-
-r = RandomWords()
-words_list = r.get_random_words()
-
-
-def loadWordsToFile():
-    with open("words.txt", 'w') as f:
-        f.write(' '.join(words_list))
-
-
-def clearWordFile():
-    open("words.txt", "w").close()
-
+# import random
+#
+# WORDS = ("python", "froid", "empreinte", "difficile", "animal",  "douche", "dire", "canard", "voiture", "voler",
+#          "habiter", "saluer", "habitation", "jouer", "vieillir", "animer", "carabine", "sorcier", "bougie",
+#          "contenir", "commentaire", "discuter", "gouter", "vendeuse", "tremblement", "aribitre", "loto", "location",
+#          "Engagement", "Quinze", "Incertain", "Acupuncture", "Cargaison", "Province", "Buffle", "Kamikaze")
+# word = random.choice(WORDS)
+#
+# def loadWordsToFile():
+#     with open("words.txt", 'w') as f:
+#         f.write(word)
+#
+#
+# def clearWordFile():
+#     open("words.txt", "w").close()
+#
+#
+# def get_random_word():
+#     with open("words.txt", "r") as f:
+#         allText = f.read()
+#         words = list(map(str, allText.split()))
+#         return random.choice(words)
+#
+#
+# loadWordsToFile()
+import urllib.request, random
+import unidecode
 
 def get_random_word():
-    with open("words.txt", "r") as f:
-        allText = f.read()
-        words = list(map(str, allText.split()))
-        return random.choice(words)
+    page = urllib.request.urlopen('http://www.pallier.org/extra/liste.de.mots.francais.frgut.txt')
+    liste_mots = page.readlines()
+    mot = random.choice(liste_mots)
+    word = mot.decode('UTF-8').split()[0]
+    page.close()
+    with open("words.txt", 'a') as f:
+        f.write(word)
 
-
-loadWordsToFile()
+    print(mot.decode('UTF-8').split()[0])
+    mot_sans_accent = unidecode.unidecode(word)
+    return mot_sans_accent
